37871a4e6f9e609f647be05ee7c84d03b7f398d7
surajrocks1997 <surajrocks1997@gmail.com>
2020-07-18 13:03:41 +0530

    crud operation added, JUNIT testing done

--- src/test/java/com/student/dal/StudentdalApplicationTests.java
+++ src/test/java/com/student/dal/StudentdalApplicationTests.java
@@ -1,13 +1,50 @@
 package com.student.dal;
 
+import java.util.Optional;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.student.dal.entities.Student;
+import com.student.dal.repos.StudentRepository;
 
 @SpringBootTest
 class StudentdalApplicationTests {
 
+	@Autowired
+	private StudentRepository studentRepository;
+	
 	@Test
-	void contextLoads() {
+	public void testCreateStudent() {
+		Student student = new Student();
+		student.setName("John");
+		student.setCourse("Java Web");
+		student.setFee(30d);
+		studentRepository.save(student);
 	}
 
+	@Test
+	public void testFindStudentById() {
+		Optional<Student> studentOptional = studentRepository.findById((long) 1);
+		if(studentOptional.isPresent()) {
+			Student student = studentOptional.get();
+			System.out.println(student);
+		}
+	}
+	
+	@Test
+	public void testUpdateStudent() {
+		Optional<Student> studentOptional = studentRepository.findById((long) 1);
+		if(studentOptional.isPresent()) {
+			Student student = studentOptional.get();
+			student.setFee(20d);
+			studentRepository.save(student);
+		}
+	}
+	
+	@Test
+	public void testDeleteStudent() {
+		studentRepository.deleteById(1L);
+	}
 }
